c5e11b73324fd7da981a49ab4ed5210f8391d245
bil10 <66562471+bil10@users.noreply.github.com>
2021-12-29 15:27:47 +0200

    Add files via upload

---
+++ 08_inputfx.py
@@ -0,0 +1,18 @@
+# input function
+import os
+print(os.listdir("C:\\Users\\bshai\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Visual Studio Code\\.vscode"))
+
+a = input("Where are you going")
+b = input("What is the name of your country")
+a = int(a)
+b = int(b)
+
+
+print(a,b)
+print(type(a),type(b))
+
+ 
+
+
+
+
